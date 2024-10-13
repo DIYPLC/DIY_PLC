abac2ccbf1647ce40c1b7551f15078f83b87d56e
VA <96442443+DIYPLC@users.noreply.github.com>
2024-10-13 17:29:24 +0300

    GNU GPL-2.0-or-later

--- src/PLC_NANO/FbModeSelector.h
+++ src/PLC_NANO/FbModeSelector.h
@@ -50,3 +50,30 @@ void FbModeSelector(struct DbModeSelector *p);
 #ifdef __cplusplus
 }
 #endif
+
+//  +---------+
+//  | GNU GPL |
+//  +---------+
+//  |
+//  |
+//  .= .-_-. =.
+// ((_/)o o(\_))
+//  `-'(. .)`-'
+//  |/| \_/ |\
+//  ( |     | )
+//  /"\_____/"\
+//  \__)   (__/
+// @COPYLEFT ALL WRONGS RESERVED :)
+// Author: VA
+// Contacts: DIY.PLC.314@gmail.com
+// Date: 2014 - 2024
+// License: GNU GPL-2.0-or-later
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+// https://www.youtube.com/watch?v=n1F_MfLRlX0
+//
+// See also:
+// https://www.youtube.com/@DIY_PLC
+// https://github.com/DIYPLC
+// https://oshwlab.com/diy.plc.314?tab=project&page=1
+// https://3dtoday.ru/blogs/diy-plc
+// https://t.me/DIY_PLC
